92977d1cf089a22698628385e4911b365b7eb305
Fernando Renan Cardoso da Silva <f.dasilva@dxc.com>
2021-08-12 19:21:28 -0300

    adiciona Layout de currículo

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
+/*!
+* Start Bootstrap - Resume v7.0.2 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+window.addEventListener('DOMContentLoaded', event => {
 
-let btnSolo = document.querySelector('#btnSolo')
-let secret = document.querySelector('#secret')
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
 
-btnSolo.addEventListener('click', fazChiste)
-let click = 0    
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
 
-function fazChiste(){
+});
 
 
-if( click ===0 ) {
-    btnSolo.innerHTML='Ou será que faz?';
-    click++;
+
+// let btnSolo = document.querySelector('#btnSolo')
+// let secret = document.querySelector('#secret')
+
+// btnSolo.addEventListener('click', fazChiste)
+// let click = 0    
+
+// function fazChiste(){
+
+
+// if( click ===0 ) {
+//     btnSolo.innerHTML='Ou será que faz?';
+//     click++;
      
-}
-else if ( click === 1){
-    btnSolo.innerHTML='Fez mesmo!';
-    click++;
+// }
+// else if ( click === 1){
+//     btnSolo.innerHTML='Fez mesmo!';
+//     click++;
      
-}
-else if( click === 2){
-    btnSolo.innerHTML='Agora acabou!';
-    click++;
+// }
+// else if( click === 2){
+//     btnSolo.innerHTML='Agora acabou!';
+//     click++;
     
-    secret.innerHTML='<p>Em breve apresento algo digno de portfolio, ou não.</p>'
+//     secret.innerHTML='<p>Em breve apresento algo digno de portfolio, ou não.</p>'
 
-}else{
-    btnSolo.innerHTML='Esse botão não faz nada!';
-    click = 0;
-}
+// }else{
+//     btnSolo.innerHTML='Esse botão não faz nada!';
+//     click = 0;
+// }
 
-}
+// }
